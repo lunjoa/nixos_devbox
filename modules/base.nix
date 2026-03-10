a5057fa87c2728de0e1c93e5d7767d33646b9a7e
lunjoa <joakim97lundstrom@gmail.com>
2026-03-10 20:23:22 +0100

    Use english

--- modules/base.nix
+++ modules/base.nix
@@ -30,7 +30,15 @@ in
 
   # Locale and timezone
   time.timeZone = "Europe/Stockholm";
-  i18n.defaultLocale = "sv_SE.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_MEASUREMENT = "sv_SE.UTF-8";
+    LC_MONETARY = "sv_SE.UTF-8";
+    LC_NUMERIC = "sv_SE.UTF-8";
+    LC_PAPER = "sv_SE.UTF-8";
+    LC_TIME = "sv_SE.UTF-8";
+  };
+  console.keyMap = "sv-latin1";
 
   # Enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
